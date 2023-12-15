7f884f096ed9743a4a30e8b61b8cf41fe9f98d24
Deepak Sharma <dsharma2828@gmail.com>
2023-12-15 13:06:49 +0530

    trying to solve encapsulation assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rwObject = new RWOnly("Initial Value");
+        // Attempting to set a value directly (error expected)
+        // rwObject.name = "DirectAccess"; // This will result in a compilation error
+        // Attempting to print the value directly (error expected)
+        // System.out.println(rwObject.name); // This will result in a compilation error
+        rwObject.setName("New Value");
+        System.out.println(rwObject.getName());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,18 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name; // Private member variable
+
+    // Constructor
+    public RWOnly(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
